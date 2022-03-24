4d524890687416902687ceb0f12add253be99415
Hritik Shah <catchmeifyoucan1080@gmail.com>
2022-03-24 05:20:09 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.template import Library
 from django.utils.html import format_html
 
+from blog.models import *
+
 register = Library()
 
 @register.filter
@@ -15,3 +17,15 @@ def author_details(author: User, current_user=None):
   name = f"{author.first_name} {author.last_name}" if author.first_name and author.last_name else author.username
 
   return format_html('<a href="mailto:{}">{}</a>', author.email, name) if author.email else name
+
+@register.simple_tag
+def row(extra_classes=''):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.inclusion_tag('blog/post-list.html')
+def recent_posts(post: Post):
+  return {'title': 'Recent Posts', 'posts': Post.objects.exclude(pk=post.pk)[: 5]}
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import *
@@ -6,3 +6,6 @@ from .models import *
 # Create your views here.
 def index(request):
   return render(request, 'blog/index.html', {'posts': Post.objects.filter(published_at__lte=timezone.now())})
+
+def post_detail(request, slug):
+  return render(request, 'blog/post-detail.html', {'post': get_object_or_404(Post, slug=slug)})
