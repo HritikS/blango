2a9bf3e863cf4f4e74893af85f92c55a2dcff3a7
Hritik Shah <catchmeifyoucan1080@gmail.com>
2022-04-09 09:42:21 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -47,4 +47,7 @@ router = DefaultRouter()
 router.register('tags', TagViewSet)
 router.register('posts', PostViewSet)
 
-urlpatterns += [path('', include(router.urls))]
+urlpatterns += [
+    path('', include(router.urls)),
+    path("posts/by-time/<str:period_name>", PostViewSet.as_view({"get": "list"}), name='posts-by-time'),
+]
